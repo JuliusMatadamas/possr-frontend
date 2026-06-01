abe6e51c9f96dce1556cbe245602447d88477408
Julio Cesar Matadamas <nem.jmatadamas@coppel.com>
2026-06-01 13:33:47 -0500

    Feat: Basic structure created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './features/auth/login/login';
+import { Logout } from './features/auth/logout/logout';
+import { NotFound } from './features/auth/not-found/not-found';
+import { Home } from './features/home/home';
+import { Layout } from './layout/layout';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Layout,
+        children: [
+            {
+                path: 'home',
+                component: Home
+            },
+            {
+                path: 'admin',
+                loadChildren: () => import('./features/admin/admin.routes').then(m => m.adminRoutes)
+            }
+        ]
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'logout',
+        component: Logout
+    },
+    {
+        path: 'not-found',
+        component: NotFound
+    }
+];
---
+++ src/app/features/admin/employee/employee.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-employee',
+    imports: [],
+    templateUrl: './employee.html',
+    styleUrl: './employee.scss',
+})
+export class Employee {}
